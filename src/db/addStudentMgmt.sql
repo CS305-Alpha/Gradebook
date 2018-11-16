87dd4d94ed60aeea6e219e13abdaf2d7d3f8598f
Andrew Figueroa <afig23@gmail.com>
2018-11-15 20:30:07 -0500

    Update src/db/addStudentMgmt.sql
    Co-Authored-By: jrm86 <jrm86git@gmail.com>

--- src/db/addStudentMgmt.sql
+++ src/db/addStudentMgmt.sql
@@ -553,7 +553,7 @@ GRANT EXECUTE ON FUNCTION getStudentSections(studentID INT,
                                           courseNumber VARCHAR(8)
                                           )
    TO alpha_GB_Webapp, alpha_GB_Instructor, alpha_GB_Registrar, 
-      alpha_GB_RegistrarAdmin, alpha_GB_Admissions, alpha_GB_DBAdmin,
+   alpha_GB_RegistrarAdmin, alpha_GB_Admissions, alpha_GB_DBAdmin,
       alpha_GB_Student;
 
 
